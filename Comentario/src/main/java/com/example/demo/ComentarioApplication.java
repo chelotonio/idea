ad3e9eab671abbd64bde3a8472cbecfe59e7fe48
franco <franco.cornejo.g@usach.cl>
2023-08-24 22:32:07 -0400

    prueba dependencias

---
+++ Comentario/src/main/java/com/example/demo/ComentarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComentarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComentarioApplication.class, args);
+	}
+
+}
