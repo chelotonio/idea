659518190d689cb9371db73a06e255fc4b718d15
franco <franco.cornejo.g@usach.cl>
2023-08-24 23:05:01 -0400

    feat: agregar servicios

---
+++ comentarioService /src/main/java/idea/comentarioService/ComentarioServiceApplication.java
@@ -0,0 +1,13 @@
+package idea.comentarioService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComentarioServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComentarioServiceApplication.class, args);
+	}
+
+}
